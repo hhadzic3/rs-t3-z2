d0f490a74fb52985a3abce06c10fe1e8dcd9f8e8
HARUN <harun.hadzic17@gmail.com>
2019-03-27 15:31:25 +0100

    zavrsen

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Pretvaranje najduze rijeci u velika slova");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
